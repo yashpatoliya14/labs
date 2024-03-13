87fa154bc3094b2269f5ad5dd8b2f15bee54dcd1
yashpatoliya14 <142727303+yashpatoliya14@users.noreply.github.com>
2024-03-12 16:02:04 -1200

    Add files via upload

--- Semester-2/java/LAB_7/Lab7b.java
+++ Semester-2/java/LAB_7/Lab7b.java
@@ -1,24 +1,66 @@
 interface A {
 
-    int a=50;
+    int a=10;
+    void method1();
 }
 interface A1 extends A{
-
+	int b=20;
+    void method2();
 }
 interface A2 extends A{
-    
+    int c=30;
+    void method3();
 }
-interface P1{
+interface  A12 {
+    int d=40;
+    void method4();
+}
+interface P1 extends A12{
+	int e=50;
+    void method5();
+}
+interface P2 extends A12{
+    int f=60;
+    void method6();
+}
+
+class B implements A1,A2,P1,P2{
+	void method1(){
+		System.out.prinln(a);
+	}
+	void method2(){
+		System.out.prinln(b);
+	}
+
+	void method3(){
+		System.out.prinln(c);
+	}
+
+	void method4(){
+		System.out.prinln(d);
+	}
+
+	void method5(){
+		System.out.prinln(e);
+	}
+	void method6(){
+		System.out.prinln(f);
+	}
+
 
 }
-interface P2{
-    
-}
-interface  A12{
-    
-}
+
 
 public class Lab7b {
    
+   public static void main(String[] args) {
+   		B b = new B();
+   		b.method1();
+   		b.method2();
+   		b.method3();
+   		b.method4();
+   		b.method5();
+   		b.method6();
+   }
     
 }
